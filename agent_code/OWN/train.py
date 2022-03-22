7f83e4d2329e087a4abf4efdd1ed5218b89ab148
Angelina Basova <65785147+aabasova@users.noreply.github.com>
2022-03-22 13:22:28 +0100

    Task 4 train
    adjusted train for compatible for all tasks

--- agent_code/OWN/train.py
+++ agent_code/OWN/train.py
@@ -6,9 +6,12 @@ import csv
 from typing import List
 
 import numpy as np
+from pandas import DataFrame
 import random
 import events as e
-from .callbacks import state_to_features
+from .callbacks import state_to_features, load_file, q_table_path, model_path, analytics_path
+from scipy.spatial.distance import cityblock
+
 
 # This is only an example!
 Transition = namedtuple('Transition',
@@ -25,19 +28,23 @@ MOVED_AWAY_FROM_COIN = "MOVED_AWAY_FROM_COIN"
 MOVED_IN_CYCLE = "MOVED_IN_CYCLE"
 NOT_MOVED_IN_CYCLE = "NOT_MOVED_IN_CYCLE"
 
-BOMB_WHEN_NO_COIN = "BOMB_WHEN_NO_COIN"
-NOT_BOMB_WHEN_NO_COIN = "NOT_BOMB_WHEN_NO_COIN"
-BOMB_IF_NO_COIN_AND_CLOSE_TO_CRATE = "BOMB_IF_NO_COIN + CLOSE_TO_CRATE"
-NOT_BOMB_IF_NO_COIN_AND_CLOSE_TO_CRATE = "NOT BOMB_IF_NO_COIN + CLOSE_TO_CRATE"
+SAFE_FROM_BOMB = 'SAFE_FROM_BOMB'
+NOT_SAFE_FROM_BOMB = "NOT_SAFE_FROM_BOMB"
 
-NOT_BOMB_WHEN_COIN_EXISTS = "NOT_BOMB_WHEN_COIN_EXISTS"
-BOMB_WHEN_COIN_EXISTS = "BOMB_WHEN_COIN_EXISTS"  # SUBJECT TO CHANGE
+MOVED_TO_CRATE = "MOVED_TO_CRATE"
+MOVED_AWAY_FROM_CRATE = "NOT_MOVE_TO_CRATE"
 
+MEANINGFUL_BOMB_DROP = "MEANINGFUL_BOMB_DROP"
+NOT_MEANINGFUL_BOMB_DROP = "NOT_MEANINGFUL_BOMB_DROP"
 MOVED_TO_BOMB = 'MOVED_TO_BOMB'
 MOVED_AWAY_FROM_BOMB = 'MOVED_AWAY_FROM_BOMB'
+DIDNT_DROP_BOMB = 'DIDNT_DROP_BOMB'
 
+MEANINGFUL_WAIT = 'MEANINGFUL_WAIT'
+NOT_MEANINGFUL_WAIT = "NOT_MEANINGFUL_WAIT"
 
-CLOSE_TO_CRATE = 'CLOSE_TO_CRATE'
+NOT_KILLED_SELF = "NOT_KILLED_SELF"
+
 
 ACTION_ROW_MAP = {
     'UP': 0,
@@ -59,25 +66,15 @@ def setup_training(self):
 
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
-    # Example: Setup an array that will note transition tuples
-    # (s, a, r, s')
     self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
 
     # ANGELINA
-    self.alpha = 0.1
-    self.gamma = 0.5
-    self.epsilon = 0.4
+    self.alpha = 0.8
+    self.gamma = 0.9
+    self.epsilon = 1
 
-    num_walls = 49
-    # free cells robot can walk or a coin can be placed
-    black_cells = 15 * 15 - num_walls
-    num_states = 7 * 7 * 7 * 7
-    num_actions = 6
-
-    if not os.path.isfile("q_table.csv"):
-        self.q_table = {(0, 0, 0, 0, 0, 0): [0, 0, 0, 0, 0, 0]}
-
-    self.training_data = np.zeros((0, 81))
+    if not os.path.isfile(q_table_path):
+        self.q_table = {}
 
     self.all_epochs, self.all_penalties = [], []
 
@@ -108,18 +105,71 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
 
     # # state_to_features is defined in callbacks.py
 
-    update_q_table(self, new_game_state, self_action, old_game_state, events)
+    update_q_table(self, new_game_state, self_action,
+                   old_game_state, events, new_game_state['step'])
+    dump_file(q_table_path, "wb", self.q_table)
 
 
-def update_q_table(self, new_game_state, self_action, old_game_state, events):
-    new_state, old_state = get_states(new_game_state, old_game_state)
+def update_q_table(self, new_game_state, self_action, old_game_state, events, step):
+    old_state, new_state = get_states(new_game_state, old_game_state)
     all_events = add_auxilliary_rewards(self, old_state, new_state, events)
+    rewards = reward_from_events(self, all_events)
+
+    # decay rewards by timestep
+    # rewards = (0.99 * np.exp(-0.01 * new_game_state['step'])) * rewards
+    rewards = rewards + potential_fct(new_state, old_state, step, self)
+    self.logger.debug(f"New reward: {rewards}")
 
     self.transitions.append(Transition(
-        old_state, self_action, new_state, reward_from_events(self, all_events)))
+        old_state, self_action, new_state, rewards))
 
     new_q_value = get_new_q_value(self)
     self.q_table[old_state][ACTION_ROW_MAP[self_action]] = new_q_value
+
+
+def potential_fct(new_state, old_state, step, self):
+    """Compute Fi for the reward shapping
+    """
+    Fi_old = fi_reward_shaping(old_state, step-1)
+    Fi_new = fi_reward_shaping(new_state, step)
+
+    F = -1 * step
+    self.logger.debug(f"F equals {F}")
+    return F
+
+
+def potential_fct_old(new_state, old_state, step, self):
+    """Compute Fi for the reward shapping
+    """
+    Fi_old = fi_reward_shaping(old_state, step-1)
+    Fi_new = fi_reward_shaping(new_state, step)
+
+    F = self.gamma*Fi_new - Fi_old
+    self.logger.debug(f"F equals {F}= ({Fi_new} - {Fi_old})")
+    return F
+
+
+def fi_reward_shaping(state, step):
+    if np.sum(np.abs(state[8:10])) != 0:
+        GOAL = list((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0))
+    else:  # no bomb exists
+        GOAL = list((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+
+    if np.sum(np.abs(state[6:8])) != 0:
+        GOAL1 = list((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0))
+        GOAL2 = list((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0))
+        if cityblock(GOAL1, state, w=[1, 1, 0, 0, 0, 0, 3, 3, 4, 4]) < cityblock(GOAL2, state, w=[1, 1, 0, 0, 0, 0, 3, 3, 4, 4]):
+            GOAL = GOAL1
+        else:
+            GOAL = GOAL2
+
+    if np.sum(np.abs(state[6:8])) == 1:
+        GOAL = list((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100.0, 100.0))
+
+    dist = cityblock(GOAL, state, w=[1, 1, 0, 0, 0, 0, 3, 3, 4, 4])
+
+    Fi = dist * 1/4 * step
+    return Fi
 
 
 def get_states(new_game_state, old_game_state):
@@ -130,18 +180,20 @@ def get_states(new_game_state, old_game_state):
     else:
         old_state = state_to_features(old_game_state)
 
-    return (new_state, old_state)
+    return (old_state, new_state)
 
 
 def get_new_q_value(self):
     old_state, self_action, new_state, rewards = self.transitions[-1]
 
     if old_state not in self.q_table:
+        # new_row = q_t_initialization(old_state)
         new_row = np.zeros(6)
         new_row[ACTION_ROW_MAP[self_action]] = rewards
         self.q_table.update({old_state: new_row})
 
     if new_state not in self.q_table:
+        # new_row = q_t_initialization(new_state)
         new_row = np.zeros(6)
         new_row[ACTION_ROW_MAP[self_action]] = rewards
         self.q_table.update({new_state: new_row})
@@ -168,53 +220,174 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
 
     :param self: The same object that is passed to all of your callbacks.
     """
-    # self.logger.debug(
-    #     f'Encountered event(s) {", ".join(map(repr, events))} in final step')
-    # self.transitions.append(Transition(state_to_features(
-    #     last_game_state), last_action, None, reward_from_events(self, events)))
+    self.logger.debug(
+        f'Encountered event(s) {", ".join(map(repr, events))} in final step')
 
     # Store the model
-    update_q_table(self, last_game_state, last_action, last_game_state, events)
+    update_q_table(self, last_game_state, last_action,
+                   last_game_state, events, last_game_state['step'])
 
-    with open("my-saved-model.pt", "wb") as file:
-        pickle.dump(self.model, file)
+    dump_file(model_path, "wb", self.model)
+    dump_file(q_table_path, "wb", self.q_table)
 
-    with open('q_table.csv', 'wb') as csv_file:
-        pickle.dump(self.q_table, csv_file, protocol=pickle.HIGHEST_PROTOCOL)
+    score = last_game_state['self'][1]
+    name = "scores" + q_table_path
+    dump_analytics(name, score)
+    # other_score = last_game_state['others'][1]
 
-    print("Close q- table: size", len(self.q_table))
 
-    # self.epsilon -= decay_rate
-    # print("NEW EPSILON: ", self.epsilon)
+def dump_file(path: str, mode: str, object):
+    with open(path, mode) as file:
+        pickle.dump(object, file)
+
+
+def dump_analytics(file_name, score):
+    path = analytics_path + file_name
+    df = DataFrame(data=[score], columns=['score'])
+    df.to_csv(path, mode='a', header=not os.path.exists(path))
 
 
 def add_auxilliary_rewards(self, old_state, new_state, events):
-    # reward for moving to nearest coin
-    old_distance = np.sum(np.abs(old_state[:2]))
-    new_distance = np.sum(np.abs(new_state[:2]))
-    if old_distance < new_distance:
-        events.append(MOVED_AWAY_FROM_COIN)
-    else:
-        events.append(MOVED_TO_COIN)
 
-    # check agent moves in cycle
+    # COINS
+    add_coin_rewards(old_state, new_state, events)
+
+    # CRATES
+    add_crate_rewards(old_state, new_state, events)
+
+    # BOMBS
+    add_bomb_rewards(old_state, new_state, events)
+
+    # rewards to incentivize good playing behavior
+    add_general_rewards(self, new_state, events)
+
+    return events
+
+
+def add_general_rewards(self, new_state, events):
+    # add valid action
+    if e.INVALID_ACTION not in events:
+        events.append(VALID_ACTION)
+
+     # check agent moves in cycle
     if(len(self.transitions) > 2):
         st1 = self.transitions[1].next_state
         st2 = self.transitions[2].next_state
         if(new_state in [st1, st2]):
             events.append(MOVED_IN_CYCLE)
-            print("in CYCLE: ", new_state)
+            # print("in CYCLE: ", new_state)
         else:
             events.append(NOT_MOVED_IN_CYCLE)
 
-    # add valid action
-    if e.INVALID_ACTION not in events:
-        events.append(VALID_ACTION)
+    # agent survived bomb
+    if e.BOMB_EXPLODED in events and e.KILLED_SELF not in events:
+        events.append(NOT_KILLED_SELF)
 
-    # drop bomb if no coins
-    # if old_distance == 0:  # no coins in sight
+    if e.WAITED in events and MEANINGFUL_WAIT not in events:
+        events.append(NOT_MEANINGFUL_WAIT)
 
-    return events
+    if e.BOMB_DROPPED in events and MEANINGFUL_BOMB_DROP not in events:
+        events.append(NOT_MEANINGFUL_BOMB_DROP)
+
+
+def add_coin_rewards(old_state, new_state, events):
+    # COINS
+    # distances correspond to manhattan distance
+    old_distance_coin = np.sum(np.abs(old_state[:2]))
+    new_distance_coin = np.sum(np.abs(new_state[:2]))
+
+    # reward for moving to nearest coin if one exists
+    if old_distance_coin != 0:
+        if old_distance_coin <= new_distance_coin:
+            if e.COIN_COLLECTED not in events:
+                events.append(MOVED_AWAY_FROM_COIN)
+        else:
+            events.append(MOVED_TO_COIN)
+
+
+def add_crate_rewards(old_state, new_state, events):
+    left, right, up, down = old_state[2:6]
+    old_distance_crate = np.sum(np.abs(old_state[6:8]))
+    old_crate_x, old_crate_y = np.abs(old_state[6:8])
+    new_distance_crate = np.sum(np.abs(new_state[6:8]))
+    new_crate_x, new_crate_y = np.abs(new_state[6:8])
+
+    old_distance_coin = np.sum(np.abs(old_state[:2]))
+    new_distance_bomb = np.sum(np.abs(new_state[8:10]))
+
+    # # move to crate when no coins visible
+    # # no coins in sight + at least one crate exists
+    if old_distance_coin == 0 and old_distance_crate != 0:
+        # didnt drop bomb
+        if new_distance_bomb == 0:
+            if old_distance_crate <= 3 and new_distance_crate > 3:
+                events.append(DIDNT_DROP_BOMB)
+            if old_distance_crate > new_distance_crate:
+                events.append(MOVED_TO_CRATE)
+            # agent moved away from crate + a bomb was not the reason
+            else:
+                events.append(MOVED_AWAY_FROM_CRATE)
+
+    if e.BOMB_DROPPED in events:
+        # placing bomb near crate is good
+        if old_crate_x <= 3 and old_crate_y == 0 or old_crate_x == 0 and old_crate_y <= 3:
+            if (old_crate_x == 0 and up == 0 or down == 0) or (old_crate_y == 0 and right == 0 or left == 0):
+                events.append(MEANINGFUL_BOMB_DROP)
+        else:
+            # too far from crate to drop a bomb
+            events.append(NOT_MEANINGFUL_BOMB_DROP)
+
+
+def add_bomb_rewards(old_state, new_state, events):
+    old_distance_bomb = np.sum(np.abs(old_state[8:10]))
+    new_distance_bomb = np.sum(np.abs(new_state[8:10]))
+
+    old_bomb_dist_x, old_bomb_dist_y = np.abs(old_state[8:10])
+    new_bomb_dist_x, new_bomb_dist_y = np.abs(new_state[8:10])
+
+    # # if a bomb exists
+    if old_distance_bomb != 0:
+
+        # case agent was standing on bomb
+        if old_distance_bomb == 200:
+
+            if new_distance_bomb != 200:
+                events.append(MOVED_AWAY_FROM_BOMB)
+                events.append(NOT_SAFE_FROM_BOMB)
+
+            # agent was standing on bomb + is still standing
+            if new_distance_bomb == 200:
+                events.append(NOT_SAFE_FROM_BOMB)
+                # choosing to drop a bomb when standing on one is an invalid action
+                if e.INVALID_ACTION in events and NOT_MEANINGFUL_BOMB_DROP not in events:
+                    events.append(NOT_MEANINGFUL_BOMB_DROP)
+                else:
+                    events.append(NOT_MEANINGFUL_WAIT)
+
+        #     # move away from dropped bomb
+        if old_distance_bomb < new_distance_bomb:
+            # outside dangerous radius of bomb
+            if (new_bomb_dist_x > 3 and new_bomb_dist_y == 0) or (new_bomb_dist_y > 3 and new_bomb_dist_x == 0):
+                events.append(SAFE_FROM_BOMB)
+            else:
+                events.append(NOT_SAFE_FROM_BOMB)
+            if (old_bomb_dist_x <= 3 and new_bomb_dist_y == 0) or (old_bomb_dist_y <= 3 and new_bomb_dist_x == 0):
+                events.append(MOVED_AWAY_FROM_BOMB)
+
+            # moved closer to bomb but wasnt standing on one
+        if old_distance_bomb >= new_distance_bomb and old_distance_bomb != 200:
+            # inside dangerous radius of bomb
+            if (new_bomb_dist_x <= 5 and new_bomb_dist_y == 0) or (new_bomb_dist_y <= 5 and new_bomb_dist_x == 0):
+                events.append(MOVED_TO_BOMB)
+                events.append(NOT_SAFE_FROM_BOMB)
+
+    #     # wait is ok if agent in safe position from a dropped bomb
+    # ANGELINA removed old_bomb_dist_x == 0 or old_bomb_dist_y == 0 and
+    # this safe from bomb not working well yet !!!!!!
+        if new_bomb_dist_x != 0 and new_bomb_dist_y != 0 and old_distance_bomb != 200:
+            if e.WAITED:
+                events.append(MEANINGFUL_WAIT)
+            events.append(SAFE_FROM_BOMB)
 
 
 def reward_from_events(self, events: List[str]) -> int:
@@ -225,30 +398,50 @@ def reward_from_events(self, events: List[str]) -> int:
     certain behavior.
     """
     game_rewards = {
-        e.COIN_COLLECTED: 50,
-        e.KILLED_OPPONENT: 50,
-        e.GOT_KILLED: -30,
-        e.SURVIVED_ROUND: 30,
-        e.INVALID_ACTION: -100,
+        # GENERAL
         VALID_ACTION: 100,
-        MOVED_TO_COIN: 5,
-        MOVED_AWAY_FROM_COIN: -5,
-        MOVED_IN_CYCLE: -5,
+        e.INVALID_ACTION: -100,
+        MOVED_IN_CYCLE: -30,
         NOT_MOVED_IN_CYCLE: 5,
 
-        BOMB_WHEN_NO_COIN: 5,
-        NOT_BOMB_WHEN_COIN_EXISTS: 5,
-        NOT_BOMB_WHEN_NO_COIN: -5,
-        BOMB_IF_NO_COIN_AND_CLOSE_TO_CRATE: 5,
-        NOT_BOMB_IF_NO_COIN_AND_CLOSE_TO_CRATE: -5,
+        # COINS
+        MOVED_TO_COIN: 5,
+        MOVED_AWAY_FROM_COIN: -5,
+        e.COIN_COLLECTED: 50,
+        e.COIN_FOUND: 20,
 
-        MOVED_TO_BOMB: -5,
-        MOVED_AWAY_FROM_BOMB: 5
+        # CRATES
+        e.CRATE_DESTROYED: 20,
+        e.SURVIVED_ROUND: 30,
+        DIDNT_DROP_BOMB: -10,
+        MOVED_TO_CRATE: 10,
+        MOVED_AWAY_FROM_CRATE: -10,
+
+        # BOMBS
+        MOVED_TO_BOMB: -50,
+        MOVED_AWAY_FROM_BOMB: 50,
+        MEANINGFUL_WAIT: 5,
+        NOT_MEANINGFUL_WAIT: -5,
+        SAFE_FROM_BOMB: 100,
+        NOT_SAFE_FROM_BOMB: -100,
+        MEANINGFUL_BOMB_DROP: 70,
+        NOT_MEANINGFUL_BOMB_DROP: -80,
+
+
+        # LIFE
+        e.KILLED_SELF: -100,
+        NOT_KILLED_SELF: 100,
+        e.KILLED_OPPONENT: 50,
+        e.OPPONENT_ELIMINATED: 50,
+        e.GOT_KILLED: -100,
     }
+    # remove duplicate events
+    events = list(set(events))
 
     reward_sum = 0
     for event in events:
         if event in game_rewards:
             reward_sum += game_rewards[event]
+
     self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
     return reward_sum
